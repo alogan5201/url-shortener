1f30d840ec71318d958826fe586c6158469f2c33
alogan5201 <andrewthomaslogan@gmail.com>
2020-08-26 15:50:57 -0400

    t

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const exphbs  = require('express-handlebars');
-const urlShortener = require('node-url-shortener');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
-const db = require('./models/index.js');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded());
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  db.Url.findAll({order: [['createdAt', 'DESC']], limit: 5})
-        .then(urlObjs => {
-          res.render('index', {
-            urlObjs: urlObjs
-          });
-        });
-});
+require('./server/routes')(app);
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
-app.post('/url', function(req, res) {
-  const url = req.body.url
-
-  urlShortener.short(url, function(err, shortUrl) {
-    db.Url.findOrCreate({where: {url: url, shortUrl: shortUrl}})
-          .then(([urlObj, created]) => {
-            res.redirect('/');
-          });
-  });
-});
-
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+module.exports = app;
